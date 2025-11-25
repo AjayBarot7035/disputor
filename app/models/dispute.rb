b367bafeaa8040553037d7dfe8e184edbd4fa72d
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:19:01 +0530

    GREEN: Add status enum to Dispute model with default open

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -1,6 +1,15 @@
 class Dispute < ApplicationRecord
   belongs_to :charge
 
+  enum :status, {
+    open: "open",
+    needs_evidence: "needs_evidence",
+    awaiting_decision: "awaiting_decision",
+    won: "won",
+    lost: "lost",
+    reopened: "reopened"
+  }
+
   validates :external_id, presence: true, uniqueness: true
 end
 
