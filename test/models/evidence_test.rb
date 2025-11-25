32d4724c4b1f02eda788cdf6ea5e764a9f606e3a
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:38:19 +0530

    RED: Add failing test for Evidence file attachment

--- test/models/evidence_test.rb
+++ test/models/evidence_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
 class EvidenceTest < ActiveSupport::TestCase
+  include ActionDispatch::TestProcess
+  
   # Disable fixtures for TDD
   def self.fixtures(*args); end
 
@@ -8,6 +10,17 @@ class EvidenceTest < ActiveSupport::TestCase
     evidence = Evidence.new
     assert_not evidence.valid?
     assert_includes evidence.errors[:dispute], "must exist"
+  end
+
+  test "should allow attaching a file" do
+    charge = Charge.create!(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    dispute = Dispute.create!(charge: charge, external_id: "dsp_123", amount_cents: 1000, currency: "USD", opened_at: Time.current)
+    evidence = Evidence.create!(dispute: dispute, kind: "document")
+    
+    file = fixture_file_upload("test/fixtures/files/sample.txt", "text/plain")
+    evidence.file.attach(file)
+    
+    assert evidence.file.attached?
   end
 end
 
