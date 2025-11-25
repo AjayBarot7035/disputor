167b15b72099c2918fa4fc3053486df339dc0269
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:15:28 +0530

    RED: Add failing test for Dispute external_id validation

--- test/models/dispute_test.rb
+++ test/models/dispute_test.rb
@@ -9,5 +9,12 @@ class DisputeTest < ActiveSupport::TestCase
     assert_not dispute.valid?
     assert_includes dispute.errors[:charge], "must exist"
   end
+
+  test "should require external_id" do
+    charge = Charge.create!(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    dispute = Dispute.new(charge: charge, amount_cents: 1000, currency: "USD")
+    assert_not dispute.valid?
+    assert_includes dispute.errors[:external_id], "can't be blank"
+  end
 end
 
