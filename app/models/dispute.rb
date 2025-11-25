408e618ae018d883ceb3cd38913c959f1a2ecdc5
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:19:47 +0530

    GREEN: Add amount_cents validation to Dispute model

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -11,5 +11,6 @@ class Dispute < ApplicationRecord
   }
 
   validates :external_id, presence: true, uniqueness: true
+  validates :amount_cents, presence: true, numericality: { greater_than: 0 }
 end
 
