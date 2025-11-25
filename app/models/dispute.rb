de647eae68762bcea14d48857c5594dc25532a80
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:20:41 +0530

    GREEN: Add currency validation to Dispute model

--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -12,5 +12,6 @@ class Dispute < ApplicationRecord
 
   validates :external_id, presence: true, uniqueness: true
   validates :amount_cents, presence: true, numericality: { greater_than: 0 }
+  validates :currency, presence: true, inclusion: { in: %w[USD] }
 end
 
