9d19aa8e186c03d16a0078c0951b8fd3bfc69c8b
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:05:32 +0530

    GREEN: Add role enum to User model with default read_only

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  enum :role, {
+    admin: 0,
+    reviewer: 1,
+    read_only: 2
+  }
+
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
 
