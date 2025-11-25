7fd893e063e07df6daa115b78d67626ac4d8b472
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:32:47 +0530

    RED: Add failing tests for CaseAction actor and action validations

--- test/models/case_action_test.rb
+++ test/models/case_action_test.rb
@@ -9,5 +9,22 @@ class CaseActionTest < ActiveSupport::TestCase
     assert_not case_action.valid?
     assert_includes case_action.errors[:dispute], "must exist"
   end
+
+  test "should require actor" do
+    charge = Charge.create!(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    dispute = Dispute.create!(charge: charge, external_id: "dsp_123", amount_cents: 1000, currency: "USD", opened_at: Time.current)
+    case_action = CaseAction.new(dispute: dispute)
+    assert_not case_action.valid?
+    assert_includes case_action.errors[:actor], "must exist"
+  end
+
+  test "should require action" do
+    charge = Charge.create!(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    dispute = Dispute.create!(charge: charge, external_id: "dsp_123", amount_cents: 1000, currency: "USD", opened_at: Time.current)
+    user = User.create!(email: "admin@example.com", password: "password123", role: :admin)
+    case_action = CaseAction.new(dispute: dispute, actor: user)
+    assert_not case_action.valid?
+    assert_includes case_action.errors[:action], "can't be blank"
+  end
 end
 
