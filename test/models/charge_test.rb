603264a6a91cacd583aee7d031664973a9b9ecb5
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:10:27 +0530

    RED: Add failing test for Charge external_id uniqueness validation

--- test/models/charge_test.rb
+++ test/models/charge_test.rb
@@ -9,5 +9,12 @@ class ChargeTest < ActiveSupport::TestCase
     assert_not charge.valid?
     assert_includes charge.errors[:external_id], "can't be blank"
   end
+
+  test "should require unique external_id" do
+    Charge.create!(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    charge = Charge.new(external_id: "chg_123", amount_cents: 1000, currency: "USD")
+    assert_not charge.valid?
+    assert_includes charge.errors[:external_id], "has already been taken"
+  end
 end
 
