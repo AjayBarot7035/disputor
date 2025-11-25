38ca774eac26bcc30327b21c0027ee1c54448bc7
AjayBarot <ajubarot@gmail.com>
2025-11-25 13:50:52 +0530

    RED: Add failing tests for WebhookEvent model (idempotency)

---
+++ test/models/webhook_event_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class WebhookEventTest < ActiveSupport::TestCase
+  # Disable fixtures for TDD
+  def self.fixtures(*args); end
+
+  test "should require event_id" do
+    webhook_event = WebhookEvent.new
+    assert_not webhook_event.valid?
+    assert_includes webhook_event.errors[:event_id], "can't be blank"
+  end
+
+  test "should require unique event_id" do
+    WebhookEvent.create!(event_id: "evt_123", event_type: "dispute.opened", payload: {})
+    
+    webhook_event = WebhookEvent.new(event_id: "evt_123", event_type: "dispute.opened", payload: {})
+    assert_not webhook_event.valid?
+    assert_includes webhook_event.errors[:event_id], "has already been taken"
+  end
+
+  test "should require event_type" do
+    webhook_event = WebhookEvent.new(event_id: "evt_123")
+    assert_not webhook_event.valid?
+    assert_includes webhook_event.errors[:event_type], "can't be blank"
+  end
+end
+
